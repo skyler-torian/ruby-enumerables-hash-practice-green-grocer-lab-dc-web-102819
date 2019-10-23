f199252ec602c99c65275968e47d3323f66acf75
Skyler Torian <skymanat7@yahoo.com>
2019-10-23 17:36:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,16 @@
-def consolidate_cart(cart)
-    result = {}
-  cart.each_with_index do |item, i|
-    item.each do |food, info|
-      if result[food]
-        result[food][:count] += 1
-      else
-        result[food] = info
-        result[food][:count] = 1
-      end
+def consolidate_cart(cart:[])
+result = {}
+cart.each_with_index do |item, i|
+  item.each do |food, info|
+    if result[food]
+      result[food][:count] += 1 
+    else
+      result[food] = info
+      result[food][:count] =  1 
     end
   end
-  result
 end
+result
 end
 
 def apply_coupons(cart, coupons)
